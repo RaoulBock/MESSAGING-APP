53c93b7d87cb96688066f74c7684f7159511a414
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-14 15:49:06 +0200

    changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,62 +11,33 @@ app.use(express.static(__dirname + "/public"));
 // Middleware to parse JSON in the request body
 app.use(bodyParser.json());
 
-// Create a connection to the MySQL database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "test",
-});
-
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database:", err);
-    return;
-  }
-  console.log("Connected to database");
-
-  // // Perform a simple query (e.g., selecting data)
-  // connection.query("SELECT * FROM your_table", (queryErr, results) => {
-  //   if (queryErr) {
-  //     console.error("Error executing query:", queryErr);
-  //     return;
-  //   }
-  //   console.log("Query results:", results);
-  // });
-
-  // Close the connection after querying
-  connection.end((endErr) => {
-    if (endErr) {
-      console.error("Error closing connection:", endErr);
-      return;
-    }
-    console.log("Connection closed");
-  });
-});
+const users = new Map(); // To store registered users
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  // Endpoint to handle the POST request for uploaded data
-  app.post("/uploadData", (req, res) => {
-    const receivedData = req.body.data; // Assuming 'data' is the key sent from the client
+  socket.on("register", ({ phoneNumber, username }) => {
+    // Store user details in the 'users' Map
+    users.set(socket.id, { phoneNumber, username });
+    console.log(`${username} registered with phone number ${phoneNumber}`);
 
-    // Do something with the received data (e.g., log it)
-    console.log("Received data:", receivedData);
-
-    // Send a response (optional)
-    res.json({ message: "Data received on the server" });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+    // You might want to emit a success event back to the client
+    socket.emit("registrationSuccess", { username });
   });
 
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg); // Broadcast the message to everyone
+
+    console.log({ user: "", message: msg });
   });
+
+  // socket.on("disconnect", () => {
+  //   if (users.has(socket.id)) {
+  //     const { username } = users.get(socket.id);
+  //     console.log(`${username} disconnected`);
+  //     users.delete(socket.id);
+  //   }
+  // });
 });
 
 server.listen(3000, () => {
