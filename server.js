cd5950da866c69cd0031b8452379503849eec190
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-14 11:51:17 +0200

    changes

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const bodyParser = require("body-parser");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 app.use(express.static(__dirname + "/public"));
+// Middleware to parse JSON in the request body
+app.use(bodyParser.json());
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  // Endpoint to handle the POST request for uploaded data
+  app.post("/uploadData", (req, res) => {
+    const receivedData = req.body.data; // Assuming 'data' is the key sent from the client
+
+    // Do something with the received data (e.g., log it)
+    console.log("Received data:", receivedData);
+
+    // Send a response (optional)
+    res.json({ message: "Data received on the server" });
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
